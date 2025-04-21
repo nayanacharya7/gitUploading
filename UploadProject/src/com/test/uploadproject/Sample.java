912eeb487086083a99c00409633d33c3d6ae72f7
ADMIN <nayna7acharya@gmail.com>
2025-04-21 13:25:26 +0530

    TTTT

--- UploadProject/src/com/test/uploadproject/Sample.java
+++ UploadProject/src/com/test/uploadproject/Sample.java
@@ -3,7 +3,7 @@ package com.test.uploadproject;
 public class Sample {
 
 	public static void main(String[] args) {
-		System.out.println("hello mmm AA DD QQ QQ");
+		System.out.println(" mmm AA DD QQ QQ");
 
 	}
 
